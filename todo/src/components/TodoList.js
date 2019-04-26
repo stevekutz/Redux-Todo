7f79a5b386240175c11346cfed20324484889cb7
stevekutz <stkutz@gmail.com>
2019-04-25 23:22:28 -0500

    set up project layout

---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoList.css';
+
+function TodoList() {
+  return (
+    <div className="App">
+     <h2> something here</h2>
+    </div>
+  );
+}
+
+export default TodoList;
