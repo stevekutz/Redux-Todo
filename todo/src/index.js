9fabe07d83bfc2560f2d5836bbc8700514fb6a9d
stevekutz <stkutz@gmail.com>
2019-04-26 22:18:40 -0500

    set up store, building reducer next

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import {createStore} from 'redux';       //
+import {Provider} from 'react-redux';
+
+import rootReducer from './reducers';
+
 import TodoList from './components/TodoList';
 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <TodoList />
+  </Provider>
+
+  , rootElement);
 
